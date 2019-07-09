929fc1a0b5b844bad6314a60017a42b824acf16a
Whitney <benjamin.whitney@linklaters.com>
2019-07-09 16:50:16 +0100

    meh

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 9cf6ad0e5c848034403e8accce52dfd41c6b35781f566aa2e2b1ca658a7d58de"
+      }
+    });
   }
   render() {
     return (
@@ -11,6 +18,7 @@ class App extends React.Component {
         <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
+    ``;
   }
 }
 
